e27e020599241e597c36497637fa58d9cfbcd804
eigenjoe <eigenjosephmendoza@gmail.com>
2022-10-17 16:06:48 -0400

    Created Employee class to encapsulate employee data and associated functionality

---
+++ company/src/com/company/Employee.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Employee {
+    private float baseSalary;
+    private float hourlyRate;
+
+    public void Employee(double baseSalary, double hourlyRate):{
+        this.baseSalary = baseSalary;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public float getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(float baseSalary) {
+        if ( baseSalary <= 0) {
+            throw new IllegalArgumentException("Base Salary cannot be less or equal to 0");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+    public float getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(float hourlyRate) {
+        if ( hourlyRate <= 0) {
+            throw new IllegalArgumentException("Hourly Rate cannot be less or equal to 0");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+
+    public float calculateWage(int extraHours) {
+        return baseSalary + ( hourlyRate * extraHours);
+    }
+}
